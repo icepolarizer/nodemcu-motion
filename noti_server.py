49d54f70fde73d75cc7250bf256ddec830fe71ea
kenixer <hyensoochoi@gmail.com>
2018-11-14 20:26:41 +0900

    first commit

---
+++ noti_server.py
@@ -0,0 +1,21 @@
+import requests
+import time
+from pygame import mixer
+
+mixer.init()
+mixer.music.load('BEEP.WAV')
+detect_flag = False
+
+while(True):
+    p = requests.get("http://192.168.1.165")
+    if p.text == 'true':
+        detected = True
+    else:
+        detected = False
+    print(detected)
+    if detected and (detect_flag == False):
+        print("Got it")
+        mixer.music.play()
+    detect_flag = detected
+    time.sleep(0.1)
+
